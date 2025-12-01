7de16bf25ddfe3da14e01d5dfea29a20cea91938
blckfrost <beataanima88@gmail.com>
2025-12-01 13:20:17 +0000

    part 2

--- 2025/day1/main.go
+++ 2025/day1/main.go
@@ -8,11 +8,11 @@ import (
 	"strconv"
 )
 
-func main() {
+func partOne() {
 	// starting position
 	pos := 50
 	// keep count of zero
-	atZero := 0
+	password := 0
 
 	// open the file
 	inputFile, err := os.Open("day1_input.txt")
@@ -30,7 +30,7 @@ func main() {
 		direction := line[0]
 		distance, err := strconv.Atoi(line[1:])
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("error converting string to int: %v\n", err)
 		}
 
 		switch direction {
@@ -38,17 +38,70 @@ func main() {
 			pos = (pos - distance + 100) % 100
 
 			if pos == 0 {
-				atZero++
+				password++
 			}
 
 		case 'R':
 			pos = (pos + distance) % 100
 			if pos == 0 {
-				atZero++
+				password++
 			}
 		}
 	}
-	fmt.Println("final position:", pos)
-	fmt.Println("password:", atZero)
 
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("final position: %v password: %v\n", pos, password)
+
+}
+
+func parTwo() {
+	pos := 50
+	password := 0
+
+	file, err := os.Open("day1_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		direction := line[0]
+		distance, err := strconv.Atoi(line[1:])
+
+		if err != nil {
+			fmt.Printf("error converting string to int: %v\n", err)
+		}
+
+		for i := 0; i < distance; i++ {
+			switch direction {
+			case 'L':
+				pos = (pos - 1 + 100) % 100
+			case 'R':
+				pos = (pos + 1) % 100
+			}
+
+			if pos == 0 {
+				password++
+			}
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("final position: %v password: %v\n", pos, password)
+
+}
+
+func main() {
+	partOne()
+	parTwo()
 }
